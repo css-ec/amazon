81c6a7e106a1fcd30d221984601ff23168b4a052
Chethan K B <61843534+Chethankb117@users.noreply.github.com>
2021-01-01 12:33:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ Hello I m writing this Line1 code in Development branch
 Hello I am in master branch
 Practising stash cmd..!
 Hello I am in development branch--updated in dev branch
+updating in Remote rep github
 
